b0c7a1d922b7f792c2ad24fa2de479573d33b555
lunaluiza <136481053+lunaluiza@users.noreply.github.com>
2023-06-16 11:48:37 -0300

    Update anal_lex.py

--- anal_lex.py
+++ anal_lex.py
@@ -1,131 +1,90 @@
 import re
-import pandas as pd
+
+def split_custom(input_str):
+    output = []
+    current_str = ""
+
+    for char in input_str:
+        if char.isalnum():
+            current_str += char
+        else:
+            if current_str:
+                output.append(current_str)
+            current_str = ""
+            output.append(char)
+    return output
+
+def concatena_strings(vetor):
+    i = 0
+    while i < len(vetor)-1:
+        if vetor[i][-1] in ['!', '=', '<', '>'] and vetor[i+1][0] == '=':
+            vetor[i] = vetor[i] + vetor[i+1]
+            vetor.pop(i+1)
+        else:
+            i += 1
+    return vetor
 
 with open("teste.c", "r") as arquivo:
     codigo = arquivo.read()
+    codigo = split_custom(codigo)
+    codigo = concatena_strings(codigo)
 
-simbolos = ['!', '@', '#', '$', '%', '&', '^', '*']
-operadores = ['+', '-', '*', '/', '=', '+=', '-=', '==', '<', '>', '<=', '>=']
-palavras_reservadas = ['def', 'int', 'float','char','if', 'else', 'for', 'while', 'main', 'void',
-		       		   'printf', 'continue', 'break', 'return', 'include']
-separadores = [' ', '	', '.', ',', '\n', ';', '(', ')', '<', '>', '{', '}', '[', ']']
+keywords = {
+        'main': 'TK_RW_MAIN',
+        'string': 'TK_RW_STRING',
+        'int': 'TK_RW_INTEGER',
+        'print': 'TK_RW_PRINT',
+        'read': 'TK_RW_SCANF',
+        'while': 'TK_RW_WHILE',
+        'if': 'TK_RW_IF',
+        'elif': 'TK_RW_ELIF',
+        'else': 'TK_RW_ELSE',
+        'def': 'TK_RW_FUNCTION',
+        'return': 'TK_RW_RETURN'
+    }
+operators = {
+        '+': 'TK_MATH_ADD',
+        '-': 'TK_MATH_SUB',
+        '*': 'TK_MATH_MUL',
+        '/': 'TK_MATH_DIV',
+        '=': 'TK_ASSIGNMENT',
+        '==': 'TK_LOGIC_EQ',
+        '!=': 'TK_LOGIC_DIF',
+        '<': 'TK_LOGIC_LT',
+        '<=': 'TK_LOGIC_LTE',
+        '>': 'TK_LOGIC_GT',
+        '>=': 'TK_LOGIC_GTE',
+        '||': 'TK_LOGIC_OR',
+        '&&': 'TK_LOGIC_AND',
+        '!': 'TK_LOGIC_NOT'
+    }
+separators = {
+        '(': 'TK_OP',
+        ')': 'TK_CP',
+        '{': 'TK_OK',
+        '}': 'TK_CK',
+        ',': 'TK_COMMA',
+        ';': 'TK_END'
+    }
+regex = {
+        'identifier': r'[a-zA-Z][a-zA-Z0-9]*',
+        'integer': r'\d+',
+        'string': r'"[a-zA-Z0-9@{}+\-*/=(),]+"'
+    }
 
-isStr = False
-isLib = False
-lexem = ''
-
-lexemas = []
 tokens = []
-identificadores = []
-constantes = []
+for word in codigo:
+        if word in keywords:
+            tokens.append(keywords[word])
+        elif re.match(regex['identifier'], word):
+            tokens.append('TK_IDENTIFIER')
+        elif re.match(regex['integer'], word):
+            tokens.append('TK_INTEGER')
+        elif re.match(regex['string'], word):
+            tokens.append('TK_STRING')
+        elif word in operators:
+            tokens.append(operators[word])
+        elif word in separators:
+            tokens.append(separators[word])
 
-# ETAPA 1
-# Coleta dos lexemas
-for i in codigo:
-	# ESTADO - TEXTO ENTRE ASPAS #
-	if i == '"' or i == "'":
-		if isStr:
-			lexemas.append(lexem)				# Assim que encontrar aspas fechando, adicionamos o lexema a lista.
-			lexem = ''
-		isStr = not isStr 						# Transformo isStr em True, e isso vai ser alterado assim que encontrar
-												# a próxima aspas fechando.
-	elif isStr:
-		lexem = lexem+i 						#Adicionando todo caractere que está depois das aspas ao lexema.
-	
-	#ESTADO - INSERÇÃO DE BIBLIOTECAS
-	elif isLib and i == '<':
-		lexemas.append(lexem)
-		lexem = ''
-		lexem = lexem+i
-		lexemas.append(lexem)
-		lexem = ''
-	
-	elif isLib and i == '.':
-		lexem = lexem + i
-
-	elif isLib and i == '>':
-		lexemas.append(lexem)
-		lexem = ''
-		lexem = lexem+i
-		lexemas.append(lexem)
-		lexem = ''
-		isLib = False
-
-	# ESTADO - É UM SÍMBOLO #
-	elif i in simbolos:
-		lexemas.append(i)
-    
-	# SEPARAÇÃO DE LEXEMAS #
-	# ESTADO - É UM SÍMBOLO OU UM OPERADOR
-	elif ((i in separadores) or (i in operadores)) and (not isLib):
-		if lexem: 								# Token está preenchido e o estado atual é um delimitador, ou seja, o nome  
-			lexemas.append(lexem)				# do token já está coletado.
-			lexem = ''
-        
-		if not (i==' ' or i=='\n' or i=='	'): # Token está vazio e o estado atual i não é espaço, nem \n e nem \t
-			lexemas.append(i)
-
-	# ESTADO - É ALFANUMÉRICO
-	elif  i.isalnum():
-		lexem = lexem+i
-
-	if i == '#':
-		isLib = True
-
-
-# ETAPA 2
-# Criação dos tokens {nome_tk, nome_tab, pos_tab}
-for lexem in lexemas:
-	if lexem in simbolos:
-		token  = {
-					"nome_tk": "tk_" + lexem,
-					"nome_tab": "simbolos",
-					"pos_tab": simbolos.index(lexem)
-				 }
-		tokens.append(token)
-
-	elif lexem in operadores:
-		token  = {
-					"nome_tk": "tk_" + lexem,
-					"nome_tab": "operadores",
-					"pos_tab": operadores.index(lexem)
-				 }
-		tokens.append(token)
-
-	elif lexem in palavras_reservadas:
-		token  = {
-					"nome_tk": "tk_" + lexem,
-					"nome_tab": "palavras_reservadas",
-					"pos_tab": palavras_reservadas.index(lexem)
-				 }
-		tokens.append(token)
-				
-	elif re.search("^[_a-zA-Z][_a-zA-Z0-9.]*$",lexem): # Expressão regular que aceita nomes de variáveis em C.
-		if lexem not in identificadores: 			  # Se não ter o lexema na tabela de id, adicionamos.
-			identificadores.append(lexem)
-		token  = {
-					"nome_tk": "tk_id",
-					"nome_tab": "identificadores",
-					"pos_tab": identificadores.index(lexem)
-				 }
-		tokens.append(token)
-		
-	elif lexem in separadores:
-		token  = {
-					"nome_tk": "tk_" + lexem,
-					"nome_tab": "separadores",
-					"pos_tab": separadores.index(lexem)
-				 }
-		tokens.append(token)
-		
-	else: # É um valor constante
-		if lexem not in constantes: # Se não ter o lexema na tabela de id, adicionamos.
-			constantes.append(lexem)
-		token  = {
-					"nome_tk": "tk_id" + lexem,
-					"nome_tab": "constantes",
-					"pos_tab": constantes.index(lexem)
-				 }
-		tokens.append(token)
 print(tokens)
-# print(*tokens, sep="\n")
